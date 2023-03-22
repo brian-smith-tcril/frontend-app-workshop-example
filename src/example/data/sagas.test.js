43031ab42d56c12d9798ff6c7a1d732b1e214929
Brian Smith <bsmith@tcril.org>
2023-03-22 12:21:03 -0400

    no logged in user

--- src/example/data/sagas.test.js
+++ src/example/data/sagas.test.js
@@ -39,56 +39,25 @@ describe('RootSaga', () => {
     });
   });
 
-  // describe('handleFetchCourses', () => {
-  //   it('should fetch courses without a logged in user', () => {
-
-  //     const action = profileActions.fetchCourses();
-  //     const gen = handleFetchCourses(action);
-
-  //     const result = [];
-
-  //     expect(gen.next().value).toEqual(select(userAccountSelector));
-  //     expect(gen.next(selectorData).value).toEqual(put(profileActions.fetchProfileBegin()));
-  //     expect(gen.next().value).toEqual(all([
-  //       call(ProfileApiService.getAccount, 'gonzo'),
-  //       call(ProfileApiService.getCourseCertificates, 'gonzo'),
-  //       call(ProfileApiService.getPreferences, 'gonzo'),
-  //     ]));
-  //     expect(gen.next(result).value)
-  //       .toEqual(put(profileActions.fetchProfileSuccess(userAccount, result[2], result[1], true)));
-  //     expect(gen.next().value).toEqual(put(profileActions.fetchProfileReset()));
-  //     expect(gen.next().value).toBeUndefined();
-  //   });
-
-  //   it('should fetch certificates and profile for some other user profile', () => {
-  //     const userAccount = {
-  //       username: 'gonzo',
-  //       other: 'data',
-  //     };
-  //     getAuthenticatedUser.mockReturnValue(userAccount);
-  //     const selectorData = {
-  //       userAccount,
-  //     };
-
-  //     const action = profileActions.fetchProfile('booyah');
-  //     const gen = handleFetchProfile(action);
-
-  //     const result = [{}, [1, 2, 3]];
-
-  //     expect(gen.next().value).toEqual(select(userAccountSelector));
-  //     expect(gen.next(selectorData).value).toEqual(put(profileActions.fetchProfileBegin()));
-  //     expect(gen.next().value).toEqual(all([
-  //       call(ProfileApiService.getAccount, 'booyah'),
-  //       call(ProfileApiService.getCourseCertificates, 'booyah'),
-  //     ]));
-  //     expect(gen.next(result).value)
-  //       .toEqual(put(profileActions.fetchProfileSuccess(result[0], {}, result[1], false)));
-  //     expect(gen.next().value).toEqual(put(profileActions.fetchProfileReset()));
-  //     expect(gen.next().value).toBeUndefined();
-  //   });
-  // });
-
   describe('handleFetchCourses', () => {
+    it('should fetch courses without a logged in user', () => {
+
+      const action = coursesActions.fetchCourses();
+      const gen = handleFetchCourses(action);
+
+      const result = [[1, 2, 3]];
+
+      expect(gen.next().value).toEqual(select(userAccountSelector));
+      expect(gen.next(selectorData).value).toEqual(put(coursesActions.fetchCoursesBegin()));
+      expect(gen.next().value).toEqual(all([
+        call(CoursesApiService.getCourses),
+      ]));
+      expect(gen.next(result).value)
+        .toEqual(put(coursesActions.fetchProfileSuccess(result[1], true)));
+      expect(gen.next().value).toEqual(put(coursesActions.fetchCoursesReset()));
+      expect(gen.next().value).toBeUndefined();
+    });
+
     it('should fetch courses for the current user profile', () => {
       const userAccount = {
         username: 'gonzo',
@@ -115,32 +84,5 @@ describe('RootSaga', () => {
       expect(gen.next().value).toEqual(put(coursesActions.fetchCoursesReset()));
       expect(gen.next().value).toBeUndefined();
     });
-
-    // it('should fetch certificates and profile for some other user profile', () => {
-    //   const userAccount = {
-    //     username: 'gonzo',
-    //     other: 'data',
-    //   };
-    //   getAuthenticatedUser.mockReturnValue(userAccount);
-    //   const selectorData = {
-    //     userAccount,
-    //   };
-
-    //   const action = profileActions.fetchProfile('booyah');
-    //   const gen = handleFetchProfile(action);
-
-    //   const result = [{}, [1, 2, 3]];
-
-    //   expect(gen.next().value).toEqual(select(userAccountSelector));
-    //   expect(gen.next(selectorData).value).toEqual(put(profileActions.fetchProfileBegin()));
-    //   expect(gen.next().value).toEqual(all([
-    //     call(ProfileApiService.getAccount, 'booyah'),
-    //     call(ProfileApiService.getCourseCertificates, 'booyah'),
-    //   ]));
-    //   expect(gen.next(result).value)
-    //     .toEqual(put(profileActions.fetchProfileSuccess(result[0], {}, result[1], false)));
-    //   expect(gen.next().value).toEqual(put(profileActions.fetchProfileReset()));
-    //   expect(gen.next().value).toBeUndefined();
-    // });
   });
 });
