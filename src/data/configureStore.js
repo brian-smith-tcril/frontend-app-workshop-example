d110611d7fe237c6879bd8bc1a80e8671d8bad81
Brian Smith <112954497+brian-smith-tcril@users.noreply.github.com>
2023-03-22 14:11:59 -0700

    feat: fetch course list from API

---
+++ src/data/configureStore.js
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { coursesApi } from '../example';
+
+const store = configureStore({
+  reducer: {
+    // Add the generated reducer as a specific top-level slice
+    [coursesApi.reducerPath]: coursesApi.reducer,
+  },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(coursesApi.middleware),
+});
+
+setupListeners(store.dispatch);
+
+export default store;
