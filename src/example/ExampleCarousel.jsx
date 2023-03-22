bfb04cd8669cf9f45eecac833b28c244ae8ea506
Brian Smith <bsmith@tcril.org>
2023-03-21 22:52:24 -0400

    lint

--- src/example/ExampleCarousel.jsx
+++ src/example/ExampleCarousel.jsx
@@ -43,19 +43,19 @@ const exampleCarouselData = [
     imageUrl: 'https://raw.githubusercontent.com/brian-smith-tcril/workshop-images/main/woman-5888934_1920.jpg',
     imageAltText: 'Stylized silhouette of a woman at a computer (pink)',
   },
-]
+];
 
 const examplePrevIcon = (
-  <img width="100px" src="https://raw.githubusercontent.com/brian-smith-tcril/workshop-images/73023da716b7b11723a2eea5d2a76a2ab944c55d/aiga_left_arrow_bg.svg" />
+  <img width="100px" alt="Previous" src="https://raw.githubusercontent.com/brian-smith-tcril/workshop-images/73023da716b7b11723a2eea5d2a76a2ab944c55d/aiga_left_arrow_bg.svg" />
 );
 
 const exampleNextIcon = (
-  <img width="100px" src="https://raw.githubusercontent.com/brian-smith-tcril/workshop-images/73023da716b7b11723a2eea5d2a76a2ab944c55d/aiga_right_arrow_bg.svg" />
+  <img width="100px" alt="Next" src="https://raw.githubusercontent.com/brian-smith-tcril/workshop-images/73023da716b7b11723a2eea5d2a76a2ab944c55d/aiga_right_arrow_bg.svg" />
 );
 
 const ExampleCarousel = () => (
   <Carousel prevIcon={examplePrevIcon} nextIcon={exampleNextIcon}>
-    {exampleCarouselData.map(carouselEntryData => 
+    {exampleCarouselData.map(carouselEntryData => (
       <Carousel.Item>
         <div class="img-gradient">
           <img
@@ -69,7 +69,7 @@ const ExampleCarousel = () => (
           <p>{carouselEntryData.description}</p>
         </Carousel.Caption>
       </Carousel.Item>  
-    )}
+    ))}
   </Carousel>
 );
 
